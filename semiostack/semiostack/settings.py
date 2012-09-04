28f0d9991ee3ed5b4286314d6069c8228f8fc394
samidarko <samidarko@gmail.com>
2012-09-04 13:59:25 +0200

    Modification du settings.py

--- semiostack/semiostack/settings.py
+++ semiostack/semiostack/settings.py
@@ -5,7 +5,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Sami Darko', 'sami@samidarko.fr'),
+    ('Sami Darko', 'samidarko@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -16,7 +16,7 @@ DATABASES = {
         'NAME': 'semiostack',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -24,10 +24,7 @@ DATABASES = {
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'Europe/Paris'
 
 # Language code for this installation. All choices can be found here:
@@ -44,9 +41,14 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = False
+
+PROJECT_ROOT = os.path.join(os.getcwd(), 'semiostack').replace('\\','/')
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__),'media').replace('\\','/')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media').replace('\\','/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -57,22 +59,19 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__),'static').replace('\\','/')
+STATIC_ROOT = os.path.join(os.getcwd()).replace('\\','/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
+STATIC_DIR = os.path.join(os.getcwd(), __package__, 'static').replace('\\','/')
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_DIR,
 )
 
 # List of finder classes that know how to find static files in
@@ -99,19 +98,25 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'semiostack.urls'
 LOGIN_URL = '/login'
 LOGOUT_URL = '/logout'
 
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'semiostack.wsgi.application'
+
+
 # Email configuration
 SERVER_EMAIL = 'contact@vedev.fr'
 EMAIL_HOST = 'smtp.bbox.fr'
-EMAIL_FILE_PATH = os.path.join(os.path.dirname(__file__),'mqueue').replace('\\','/')
+EMAIL_FILE_PATH = os.path.join(PROJECT_ROOT, 'mqueue').replace('\\','/') 
 
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__),'templates').replace('\\','/'),
+    os.path.join(PROJECT_ROOT, 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
